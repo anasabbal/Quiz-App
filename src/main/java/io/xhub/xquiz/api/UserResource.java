dc5e50c16831c9fcd430c4fc9e03946e42ad37ae
unknown <anouarensa2017@gmail.com>
2022-08-04 10:32:05 +0100

    fix

--- src/main/java/io/xhub/xquiz/api/UserResource.java
+++ src/main/java/io/xhub/xquiz/api/UserResource.java
@@ -1,19 +1,14 @@
 package io.xhub.xquiz.api;
 
 import io.xhub.xquiz.command.UserCommand;
-import io.xhub.xquiz.dto.ResponseDTO;
-import io.xhub.xquiz.feign.AttendeeFeignClient;
 import io.xhub.xquiz.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static io.xhub.xquiz.constants.ResourcePath.USERS;
 import static io.xhub.xquiz.constants.ResourcePath.V1;
