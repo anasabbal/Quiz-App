f446091ec6940fab4071dcdc1ffe965d80faa0cc
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-08-04 11:34:43 +0100

    fix

--- src/main/java/io/xhub/xquiz/api/EventResource.java
+++ src/main/java/io/xhub/xquiz/api/EventResource.java
@@ -34,7 +34,7 @@ public class EventResource {
     }
 
 
-    @PostMapping("/{id}/sessions")
+    @PostMapping("/sessions")
     public ResponseEntity createSession(
             @RequestBody final CreateEventSessionCommand body
     ) {
