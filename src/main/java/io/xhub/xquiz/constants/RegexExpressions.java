7fce42c6b13fa1b9290067d2f443390cf5ced7dc
Marwane Jemhour <jemhourmarwane@gmail.com>
2022-09-09 16:43:05 +0000

    FIX[XQ-257]:fix background

--- src/main/java/io/xhub/xquiz/constants/RegexExpressions.java
+++ src/main/java/io/xhub/xquiz/constants/RegexExpressions.java
@@ -4,5 +4,5 @@ public class RegexExpressions {
 
     public static final String ALPHABETIC_MIN_2_CHARS = "^([A-Za-z_ ]){2,}$";
     public static final String PHONE_NUMBER = "(\\+\\d{1,3})(\\d{9})$";
-    public static final String EMAIL = "[a-z0-9]+@[a-z]+\\.[a-z]{2,3}";
+    public static final String EMAIL = "^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$";
 }
