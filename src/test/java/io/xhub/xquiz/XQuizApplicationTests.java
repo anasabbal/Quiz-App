344b5a8f2de518eda0215675a8f17004e8c72832
Marwane Jemhour <jemhourmarwane@gmail.com>
2022-09-13 21:20:20 +0000

    FIX[XQ-227]: create quiz test fix

--- src/test/java/io/xhub/xquiz/XQuizApplicationTests.java
+++ src/test/java/io/xhub/xquiz/XQuizApplicationTests.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 @AutoConfigureMessageVerifier
 public class XQuizApplicationTests {
 
