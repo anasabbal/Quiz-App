503fadf39981fb1da21cbb0a37d9d502f30e09f6
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-09-22 04:30:52 +0100

    FEAT[XQ-338]: Remove sub theme

--- src/main/java/io/xhub/xquiz/service/question/QuestionServiceImpl.java
+++ src/main/java/io/xhub/xquiz/service/question/QuestionServiceImpl.java
@@ -36,7 +36,12 @@ public class QuestionServiceImpl implements QuestionService {
         final List<List<Question>> questions = new ArrayList<>();
 
         subThemes.forEach(subTheme -> log.info("Begin creating list of question from sub theme id {} and percentage ", subTheme.getId(), subTheme.getPercentage()));
-        subThemes.forEach(subTheme -> questions.add(questionRepository.findListQuestionBySeniorityLevelIdAndSubThemeId(command.getSeniorityLevelId(), subTheme.getId(), subTheme.getPercentage() * totalQuestions /100)));
+        subThemes.forEach(subTheme -> questions.
+                add(questionRepository.
+                        findListQuestionBySeniorityLevelIdAndSubThemeId(
+                                command.getSeniorityLevelId(),
+                                subTheme.getId(),
+                                subTheme.getPercentage() * totalQuestions /100)));
         subThemes.forEach(subTheme -> log.info("List of question created successfully for sub theme id {} and percentage ", subTheme.getId(), subTheme.getPercentage()));
         log.info("Question with size {} created successfully", questions.size());
        return questions.stream()
