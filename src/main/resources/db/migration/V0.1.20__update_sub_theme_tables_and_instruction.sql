6acf566048269fd6962b66261273a61f06c2401e
Anas Ouzoline <87352192+anasabbal@users.noreply.github.com>
2022-09-27 09:45:23 +0100

    FEAT:[XQ-349]: add questions to db(BE, DevOps, UI/UX, Mobile)

---
+++ src/main/resources/db/migration/V0.1.20__update_sub_theme_tables_and_instruction.sql
@@ -0,0 +1,25 @@
+update quiz_instructions
+SET value = '10'
+WHERE id in ('1');
+
+UPDATE sub_themes
+SET percentage = 40
+WHERE id in ('13', '14');
+
+UPDATE sub_themes
+SET percentage = 10
+WHERE id in ('22', '21', '24');
+
+UPDATE sub_themes
+SET percentage = 20
+WHERE id in ('15', '20');
+
+UPDATE sub_themes
+SET percentage = 100
+WHERE id in ('16', '17', '18', '23', '25');
+
+UPDATE sub_themes
+SET percentage = 30
+WHERE id in ('19');
+
+
